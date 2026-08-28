8c4c513b883eb9abb248e4bd72a84b5197f67e66
sergashtromb <schtrom2004@mail.ru>
2026-08-28 08:23:19 +0700

    relocate rate pkg

--- rate/rate.go
+++ rate/rate.go
@@ -27,9 +27,28 @@ func New(capacity float64, fillsp int16) *Limiter {
 
 func (lim *Limiter) Allow() bool {
 
+	lim.updateTokens()
+
 	lim.mu.Lock()
 	defer lim.mu.Unlock()
-	// TODO make update limiter
+	
+	if lim.tokens >= 1 {
+		lim.tokens -= 1
+		return true
+	} else {
+		return false
+	}
 
-	return false
+}
+
+func (lim *Limiter) updateTokens() {
+
+	lim.mu.Lock()
+	defer lim.mu.Unlock()
+
+	now := time.Now()
+	elapsed := now.Sub(time.Unix(lim.lastTime, 0)).Seconds()
+
+	lim.tokens += elapsed * float64(lim.fillsp)
+
 }
